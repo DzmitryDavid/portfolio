3ab0e804e5748a1891281d86665ad9fed75fb087
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-02-06 00:01:32 +0300

    adaptive promo about sections

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,12 @@ const burger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
+const pageUp = document.querySelector('.pageup');
+const aboutLink = document.querySelector('.promo__link');
+const worksLink = document.querySelector('[works]');
 
-console.log(overlay);
+console.log(worksLink);
+
 burger.addEventListener('click', () =>{
     menu.classList.add('active');
 });
@@ -18,5 +22,35 @@ overlay.addEventListener('click', (e) => {
         menu.classList.remove('active');
     }
 });
+
+function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        pageUp.style.display = 'block';
+    }
+    if (scrolled < 1650) {
+        pageUp.style.display = 'none';
+    
+    }
+}
+
+window.addEventListener('scroll', trackScroll);
+pageUp.addEventListener('click', () => {
+    scrollTo(0, 0)
+});
+
+window.addEventListener('mouseover',(e) => {
+    if (e.target === worksLink) {
+        aboutLink.style.color = '#fff';
+        aboutLink.style.boxShadow = 'none';
+    } else {
+        aboutLink.style.color = '#ff133e';
+        aboutLink.style.boxShadow = '0 5px 0 #ff133e';
+    }
+    
+});
+
 
 
